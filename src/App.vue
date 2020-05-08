4c1f465f8f0384339e3c0ac33956361195c7065b
Priya <priya.danu@spicemoney.com>
2020-05-08 17:36:18 +0530

    axios

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+   
+    <ListContacts />
+  </div>
+</template>
+
+<script>
+import ListContacts from './components/ListContacts.vue'
+
+export default {
+  name: 'App',
+  components: {
+    ListContacts
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
